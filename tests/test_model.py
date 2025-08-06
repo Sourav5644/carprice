ad6a83e22471c98b49110d3631f3fd21c95b3039
Sourav <bhardwajsouav113@gmail.com>
2025-08-07 00:26:48 +0530

    cicd9

--- tests/test_model.py
+++ tests/test_model.py
@@ -18,8 +18,8 @@ class TestModelLoading(unittest.TestCase):
         os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
         dagshub_url = "https://dagshub.com"
-        repo_owner = "vikashdas770"
-        repo_name = "YT-Capstone-Project"
+        repo_owner = "Sourav5644"
+        repo_name = "carprice"
 
         mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
