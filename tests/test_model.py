05398783462a60e98496d70ef862be9810e823a2
Sourav <bhardwajsouav113@gmail.com>
2025-08-07 00:46:15 +0530

    cicd10

--- tests/test_model.py
+++ tests/test_model.py
@@ -9,7 +9,6 @@ class TestModelLoading(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        # Set up DagsHub credentials for MLflow tracking
         dagshub_token = os.getenv("CAPSTONE_TEST")
         if not dagshub_token:
             raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
@@ -23,16 +22,15 @@ class TestModelLoading(unittest.TestCase):
 
         mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
-        # Load model from MLflow
         cls.new_model_name = "my_model"
         cls.new_model_version = cls.get_latest_model_version(cls.new_model_name)
         cls.new_model_uri = f'models:/{cls.new_model_name}/{cls.new_model_version}'
         cls.new_model = mlflow.pyfunc.load_model(cls.new_model_uri)
 
-        # Load the preprocessor
-        cls.preprocessor = pickle.load(open('data/processed/preprocessor.pkl', 'rb'))
+        # Safe file load
+        with open('data/processed/preprocessor.pkl', 'rb') as f:
+            cls.preprocessor = pickle.load(f)
 
-        # Load test data
         cls.holdout_data = pd.read_csv('data/processed/test_processed.csv')
 
     @staticmethod
@@ -66,23 +64,19 @@ class TestModelLoading(unittest.TestCase):
 
         self.assertEqual(input_data.shape[1], len(self.preprocessor.get_feature_names_out()))
         self.assertEqual(len(prediction), input_data.shape[0])
-        self.assertEqual(len(prediction.shape), 1)  # For regression output
+        self.assertEqual(len(prediction.shape), 1)
 
     def test_model_performance(self):
         X_holdout = self.holdout_data.iloc[:, :-1]
         y_holdout = self.holdout_data.iloc[:, -1]
 
-        # Apply preprocessing
-        X_transformed = self.transform(X_holdout)
-
-        # Predict
+        X_transformed = self.preprocessor.transform(X_holdout)
         y_pred = self.new_model.predict(X_transformed)
 
-        # Evaluate
         r2 = r2_score(y_holdout, y_pred)
 
         expected_r2_score = 0.40
-        self.assertGreaterEqual(r2, expected_r2_score, f'R2 score should be at least {expected_r2_score}')
+        self.assertGreaterEqual(r2, expected_r2_score, f'R2 score should be at least {expected_r2_score}, but got {r2:.2f}')
 
 if __name__ == "__main__":
     unittest.main()
